f18612f1822402df86e04c10c6e425514406c4d3
Krystian Frączek <krystian.fraczek@microsoft.wsei.edu.pl>
2024-04-03 23:54:46 +0200

    Initial commit

---
+++ src/main/java/pl/sklepPapierniczy/MyShop/MyShopApplication.java
@@ -0,0 +1,13 @@
+package pl.sklepPapierniczy.MyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sklepPapierniczy/MyShop/MyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sklepPapierniczy.MyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
